4abeebfdac3aa4b90fb59f499897da9453cac626
JsonDoe <128181105+JsonDoe@users.noreply.github.com>
2023-04-11 21:35:43 +0200

    Add files via upload

---
+++ AutoRigRawCodes/tools/leftHandRotationSpaceSwitch.py
@@ -0,0 +1,45 @@
+import maya.cmds as cmds 
+
+#leftHandRotationSpaceSwitch
+nspc="L_arm"
+
+cmds.namespace( set=f"{nspc}")
+
+cmds.parentConstraint(f"{nspc}:clavicleRotationSpace_BUF", f"{nspc}:ikHandSpaceSwitch_BUF", n="leftHandSpaceSwitch_paCns", mo= True),
+cmds.parentConstraint("spine:chestRotationSpace_BUF", f"{nspc}:ikHandSpaceSwitch_BUF", n="leftHandSpaceSwitch_paCns", mo= True)
+cmds.parentConstraint("spine:upperBodyRotationSpace_BUF", f"{nspc}:ikHandSpaceSwitch_BUF", n="leftHandSpaceSwitch_paCns", mo= True)
+cmds.parentConstraint("head:headSpaceSwitchSender_BUF", f"{nspc}:ikHandSpaceSwitch_BUF", n="leftHandSpaceSwitch_paCns", mo= True)
+cmds.parentConstraint("SRT:local_CON", f"{nspc}:ikHandSpaceSwitch_BUF", n="leftHandSpaceSwitch_paCns", mo= True)
+
+cmds.createNode("floatLogic", n="checkHandSpaceStatus1")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus1.operation", 0)
+cmds.connectAttr("SRT:setup_BUF.leftHandRotationSpaceSwitch",f"{nspc}:checkHandSpaceStatus1.floatA")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus1.floatB", 1 )
+cmds.connectAttr(f"{nspc}:checkHandSpaceStatus1.outBool", f"{nspc}:leftHandSpaceSwitch_paCns.clavicleRotationSpace_BUFW0" )
+
+cmds.createNode("floatLogic", n="checkHandSpaceStatus2")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus2.operation", 0)
+cmds.connectAttr("SRT:setup_BUF.leftHandRotationSpaceSwitch",f"{nspc}:checkHandSpaceStatus2.floatA")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus2.floatB", 2 )
+cmds.connectAttr(f"{nspc}:checkHandSpaceStatus2.outBool", f"{nspc}:leftHandSpaceSwitch_paCns.chestRotationSpace_BUFW1" )
+
+cmds.createNode("floatLogic", n="checkHandSpaceStatus3")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus3.operation", 0)
+cmds.connectAttr("SRT:setup_BUF.leftHandRotationSpaceSwitch",f"{nspc}:checkHandSpaceStatus3.floatA")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus3.floatB", 3 )
+cmds.connectAttr(f"{nspc}:checkHandSpaceStatus3.outBool", f"{nspc}:leftHandSpaceSwitch_paCns.upperBodyRotationSpace_BUFW2" )
+
+cmds.createNode("floatLogic", n="checkHandSpaceStatus4")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus4.operation", 0)
+cmds.connectAttr("SRT:setup_BUF.leftHandRotationSpaceSwitch",f"{nspc}:checkHandSpaceStatus4.floatA")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus4.floatB", 4 )
+cmds.connectAttr(f"{nspc}:checkHandSpaceStatus4.outBool", f"{nspc}:leftHandSpaceSwitch_paCns.headSpaceSwitchSender_BUFW3" )
+
+cmds.createNode("floatLogic", n="checkHandSpaceStatus5")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus5.operation", 0)
+cmds.connectAttr("SRT:setup_BUF.leftHandRotationSpaceSwitch",f"{nspc}:checkHandSpaceStatus5.floatA")
+cmds.setAttr(f"{nspc}:checkHandSpaceStatus5.floatB", 5 )
+cmds.connectAttr(f"{nspc}:checkHandSpaceStatus5.outBool", f"{nspc}:leftHandSpaceSwitch_paCns.local_CONW4")
+
+cmds.namespace( set=":")
+
